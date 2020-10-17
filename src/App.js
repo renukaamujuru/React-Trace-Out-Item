06db28465d9707174cc4cbd0add755051ecc2afb
renukaamujuru <renuka.a@avplat.com>
2020-10-17 20:38:26 +0530

    Misc changes done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import CardList from './Components/Card-list/Card-list.component';
+import {CardList} from './Components/Card-list/Card-list.component';
+import {SearchBox} from './Components/Search-box/Search-box.component';
 import './App.css';
 
 class App extends React.Component {
@@ -16,15 +17,17 @@ class App extends React.Component {
       .then(response => response.json())
       .then(photos => this.setState({ monsters: photos }));
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value }); 
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonster = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField));
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <React.Fragment>
         <h1 className="header">Monsoon Monsters Land</h1>
-        <input className="Search-box" type="search" placeholder="Search Monster"
-          onChange={e => this.setState({ searchField: e.target.value })} />
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange}/>
         <div className="Card-list">
           <CardList monsters={filteredMonster} />
         </div>
--- src/Components/Card/Card.component.jsx
+++ src/Components/Card/Card.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './../../App.css';
 
 export const Card = (props) => {
-    console.log("card", props)
     return (
         <div className="Card">
             <img alt={props.monster.name} src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
@@ -12,4 +11,3 @@ export const Card = (props) => {
 
     )
 }
-export default Card;
